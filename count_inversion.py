4bfe76275ee7fd50717955d1308798750c74fdfe
Qifan Zhang <firefighter-eric@users.noreply.github.com>
2021-01-12 22:54:09 +0800

    Update count_inversion.py

--- count_inversion.py
+++ count_inversion.py
@@ -1,24 +1,38 @@
-def count_inversion(array: list) -> int:
-    l = len(array)
-    if l == 1:
-        return 0
-    arr1 = array[:l // 2]
-    arr2 = array[l // 2:]
+def merge(arr1: list, arr2: list) -> (int, list):
+    """count inversions between arrays and merge sorted arrays
+
+    Args:
+        arr1 (list): array 1
+        arr2 (list): array 2
+
+    Returns:
+        int, list: inversions, merged array
+    """
+    arr = []
     i1 = i2 = 0
     count = 0
     while i1 < len(arr1) and i2 < len(arr2):
         if arr1[i1] <= arr2[i2]:
+            arr.append(arr1[i1])
             i1 += 1
         else:
-            print(arr1[i1], arr2[i2])
-            count += 1
+            arr.append(arr2[i2])
             i2 += 1
-    for i in range(i2, len(arr2)):
-        if arr2[i] < arr1[-1]:
-            print(arr1[-1], arr2[i2])
-            count += 1
+            count += len(arr1) - i1
+    arr += arr1[i1:]
+    arr += arr2[i2:]
+    return count, arr
 
-    return count_inversion(arr1) + count_inversion(arr2) + count
+def count_inversion(array: list) -> (int, list):
+    l = len(array)
+    if l == 1:
+        return 0, array
+
+    count1, arr1 = count_inversion(array[:l // 2])
+    count2, arr2 = count_inversion(array[l // 2:])
+    count12, arr  = merge(arr1, arr2)
+
+    return count1 + count2 + count12, arr
 
 
 if __name__ == '__main__':
